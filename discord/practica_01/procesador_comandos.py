dbceb7107c4ac61c2919ff425f8ab96acd508ecb
Alexa <alexaalarcon71@gmail.com>
2026-04-28 07:56:36 -0600

    comandos v2

--- discord/practica_01/procesador_comandos.py
+++ discord/practica_01/procesador_comandos.py
@@ -1,9 +1,27 @@
 import datetime
 def obtener_saludo(nombre_bot):
+    """
+    retorna un saludo formateado
+    """
+    return f"Hola, soy {nombre_bot} y estoy listo para ayudarte"
 
-def procesar_comando_recordar(argumento):
+def procesar_comando_recordar(comando):
+    """
+    Valida y procesa la acción de recordar un dato
+    """
+    if not comando:
+        return "Error: falta el nombre. Uso !recordar [nombre]"
+    else:
+        return f"Entendido! Recordaré el nombre: {comando}"
 
 def calcular_uptime(hora_inicio):
+    """
+    Calculan la diferencia de tiempo entre el inicio y el actual (mostrar actividad del bot)
+    """
+    ahora = datetime.datetime.now()
+    diferencia = ahora - hora_inicio
+    segundos = int(diferencia.total_seconds())
+    return f"Tiempo de actividad {segundos} segundos"
 
 def mostrar_ayuda():
 
