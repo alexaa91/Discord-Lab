7ce9b9e068dacab4cbf728dd27077576bbb65f01
Alexa <alexaalarcon71@gmail.com>
2026-04-28 07:32:40 -0600

    comandos

---
+++ discord/practica_01/procesador_comandos.py
@@ -0,0 +1,20 @@
+import datetime
+def obtener_saludo(nombre_bot):
+
+def procesar_comando_recordar(argumento):
+
+def calcular_uptime(hora_inicio):
+
+def mostrar_ayuda():
+
+def iniciar_agente():
+
+def main():
+    procesar_comando_recordar()
+    procesar_comando_recordar()
+    calcular_uptime()
+    mostrar_ayuda()
+    iniciar_agente()
+
+if __name__ == "__main__":
+    main()
